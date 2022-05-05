4ee4ce8b0e4d88ab16d999cef2bbb69cabc43ce0
kjsteinsapir <kjsteinsapir@uc.cl>
2022-05-05 15:06:01 -0400

    Empezando armar figuras

--- a.rb
+++ a.rb
@@ -7,8 +7,26 @@ fig = gets.chomp.to_i
 switch = true
 
 while switch == true
+    if fig<=3 && fig>=1
+        puts "Ingrese el número de filas de la figura"
+        num = gets.chomp.to_i
+    end
+
     if fig == 1 
         puts "Cuadrado"
+        num.times
+        puts
+            num.times
+            print("*")
+        switch = false
+    elsif fig == 2 
+        puts "Triangulo"
+        switch = false
+    elsif fig == 3 
+        puts "Piramide"
+        switch = false
+    elsif fig == 4 
+        puts "Has salido. No vamos a creas más figuras"
         switch = false
     else
         puts "La opción no es válida. Ingrese una opción válida"
